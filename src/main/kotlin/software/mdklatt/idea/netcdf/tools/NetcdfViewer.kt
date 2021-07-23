6bebfaad966aa67a3789f4430386e3eed843caab
Michael Klatt <mdklatt@alumni.ou.edu>
2021-07-22 23:57:12 -0500

    Refactored Data tab to use DataTableModel class

--- src/main/kotlin/software/mdklatt/idea/netcdf/tools/NetcdfViewer.kt
+++ src/main/kotlin/software/mdklatt/idea/netcdf/tools/NetcdfViewer.kt
@@ -15,6 +15,13 @@ import com.intellij.ui.content.ContentManagerEvent
 import com.intellij.ui.content.ContentManagerListener
 import com.intellij.ui.layout.panel
 import com.intellij.ui.table.JBTable
+import ucar.ma2.DataType
+import ucar.nc2.Dimension
+import ucar.nc2.NetcdfFile
+import ucar.nc2.Variable
+import ucar.nc2.time.Calendar
+import ucar.nc2.time.CalendarDateUnit
+import vendor.tandrial.itertools.cartProd
 import java.awt.Font
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.UnsupportedFlavorException
@@ -25,6 +32,7 @@ import java.io.File
 import java.io.IOException
 import javax.swing.*
 import javax.swing.event.ListSelectionEvent
+import javax.swing.table.AbstractTableModel
 import javax.swing.table.DefaultTableCellRenderer
 import javax.swing.table.DefaultTableModel
 
@@ -33,6 +41,7 @@ import javax.swing.table.DefaultTableModel
  * NetCDF viewer display.
  */
 class NetcdfToolWindow: ToolWindowFactory, DumbAware {
+
     /**
      * File schema content.
      */
@@ -59,6 +68,8 @@ class NetcdfToolWindow: ToolWindowFactory, DumbAware {
                         event.acceptDrop(DnDConstants.ACTION_COPY)
                         val accepted = event.transferable.getTransferData(DataFlavor.javaFileListFlavor)
                         val file = (accepted as? List<*>)?.get(0) as File
+                        ncFile?.close()
+                        ncFile = NetcdfFile.open(file.path)
                         reader.open(file.path)
                         load()
                         varNames = emptyList()
@@ -131,7 +142,7 @@ class NetcdfToolWindow: ToolWindowFactory, DumbAware {
     /**
      * Variable data content.
      */
-    inner class DataTab : JBTable(DefaultTableModel()) {
+    inner class DataTab : JBTable(DataTableModel()) {
         init {
             emptyText.text = "Select variable(s) in Schema tab"
             autoCreateRowSorter = true
@@ -144,12 +155,8 @@ class NetcdfToolWindow: ToolWindowFactory, DumbAware {
             if (displayedVarNames == varNames) {
                 return  // selected variables are already displayed
             }
-            reader.setView(varNames)
-            val model = this.model as DefaultTableModel
-            model.setDataVector(emptyArray(), emptyArray())
-            model.setColumnIdentifiers(reader.columns)
-            val maxRows = 100_000 // TODO: use pagination
-            reader.rows(0, maxRows).forEach { model.addRow(it) }
+            val model = this.model as DataTableModel
+            model.setData(ncFile!!, varNames.asSequence())
             displayedVarNames = varNames
             formatColumns()
             return
@@ -186,6 +193,7 @@ class NetcdfToolWindow: ToolWindowFactory, DumbAware {
 
     private var displayedVarNames = emptyList<String>()
     private var reader = NetcdfReader()
+    private var ncFile: NetcdfFile? = null
     private var schemaTab = SchemaTab()
     private var dataTab = DataTab()
     private var varNames = emptyList<String>()
@@ -219,6 +227,7 @@ class NetcdfToolWindow: ToolWindowFactory, DumbAware {
             it.description = "Selected variables"
             window.contentManager.addContent(it)
         }
+        window.contentManager
         return
     }
 }
@@ -248,6 +257,153 @@ private class ErrorDialog(private val message: String) : DialogWrapper(false) {
 }
 
 
-private class BoldCellRenderer : DefaultTableCellRenderer() {
+internal class DataTableModel() : AbstractTableModel() {
 
+    companion object {
+        /**
+         * Test if a variable appears to be a time variable.
+         *
+         * @param variable: variable to test
+         * @return: true if this appears to be a time variable
+         */
+        private fun isTime(variable: Variable) : Boolean {
+            // TODO: Test units attribute.
+            val name = variable.fullNameEscaped.split("/").last()
+            return name.startsWith("time", 0) && variable.dataType.isNumeric
+        }
+
+        /**
+         * Convert time variable values to ISO 8601 strings.
+         *
+         * @param variable: time variable
+         * @return: sequence of time strings
+         */
+        private fun timeValues(variable: Variable) : Sequence<String> {
+            val calendar = variable.findAttribute("calendar")?.stringValue ?: Calendar.getDefault().name
+            val units = CalendarDateUnit.of(calendar, variable.unitsString)
+            fun timeValue(value: Any) : String {
+                return units.makeCalendarDate(value.toString().toDouble()).toString()
+            }
+            val iter = variable.read().indexIterator
+            return generateSequence {
+                if (iter.hasNext()) timeValue(iter.objectNext) else null
+            }
+        }
+    }
+
+    private val logger = Logger.getInstance(this::class.java)  // runtime class resolution
+    private var indexes = emptyList<IntArray>()
+    private var variables = emptyList<Variable>()
+    private var coordinates = emptyList<List<*>>()
+    private var columnNames = emptyList<String>()
+    private var readShape = IntArray(0)
+
+    /**
+     * Returns the number of rows in the model. A
+     * `JTable` uses this method to determine how many rows it
+     * should display.  This method should be quick, as it
+     * is called frequently during rendering.
+     *
+     * @return the number of rows in the model
+     * @see .getColumnCount
+     */
+    override fun getRowCount() = indexes.size
+
+    /**
+     * Returns the number of columns in the model. A
+     * `JTable` uses this method to determine how many columns it
+     * should create and display by default.
+     *
+     * @return the number of columns in the model
+     * @see .getRowCount
+     */
+    override fun getColumnCount() = columnNames.size
+
+
+    /**
+     *
+     */
+    override fun getColumnClass(columnIndex: Int): Class<*> {
+        return if (columnIndex < coordinates.size) {
+            coordinates[columnIndex].firstOrNull()?.javaClass ?: throw IllegalArgumentException("Empty data model")
+        } else {
+            val variableIndex = columnIndex - coordinates.size
+            variables[variableIndex].dataType.primitiveClassType
+        }
+    }
+
+    /**
+     * Get the name label for a column.
+     *
+     */
+    override fun getColumnName(column: Int) = columnNames[column]
+
+    /**
+     * Returns the value for the cell at `columnIndex` and
+     * `rowIndex`.
+     *
+     * @param   rowIndex        the row whose value is to be queried
+     * @param   columnIndex     the column whose value is to be queried
+     * @return  the value Object at the specified cell
+     */
+    override fun getValueAt(rowIndex: Int, columnIndex: Int): Any {
+        return if (columnIndex < coordinates.size) {
+            // Coordinate columns.
+            val coordIndex = indexes[rowIndex][columnIndex]
+            coordinates[columnIndex][coordIndex] ?: throw IllegalStateException("Unexpected null value")
+        } else {
+            // Data columns.
+            val variableIndex = columnIndex - coordinates.size
+            variables[variableIndex].read(indexes[rowIndex], readShape).getObject(0)
+        }
+    }
+
+    /**
+     * Set the model data.
+     *
+     * The model defines columns consisting of one or more netCDF variables and
+     * their corresponding dimension coordinates. All selected variable must
+     * have congruent dimensions.
+     *
+     * @param file: open netCDF file
+     * @param varNames: variable names to use
+     */
+    fun setData(file: NetcdfFile, varNames: Sequence<String>) {
+        // TODO: Verify that all variables have the same dimensions.
+        variables = varNames.map {
+            file.findVariable(it) ?: throw IllegalArgumentException("Unknown variable: '${it}'")
+        }.toList()
+        val dimensions = variables.firstOrNull()?.dimensions ?: emptyList()
+        val dimsShape = dimensions.map { it.length }.toIntArray()
+        val axes = dimsShape.map { (0 until it) }.toTypedArray()
+        indexes = if (axes.isEmpty()) emptyList() else cartProd(*axes).map { it.toIntArray() }.toList()
+        coordinates = dimensions.map { getCoordinates(file, it).toList() }
+        readShape = IntArray(coordinates.size) { 1 }
+        columnNames = dimensions.map { it.fullNameEscaped } + variables.map {it.fullNameEscaped}
+        fireTableStructureChanged()
+    }
+
+    /**
+     * Get coordinate values for a dimension.
+     *
+     * The dimension's coordinate variable is used if one is defined, otherwise
+     * the coordinates are the dimensions indexes. Coordinates that appear to
+     * be time values are converted to ISO 8601 strings.
+     *
+     * @param file: open netCDF file
+     * @param dimension: dimension defined in the given file
+     * @return: dimensions coordinate values
+     */
+    private fun getCoordinates(file: NetcdfFile, dimension: Dimension): Sequence<*> {
+        val variable = file.findVariable(dimension.fullNameEscaped)
+        return if (variable?.isCoordinateVariable != true) {
+            // No coordinate variable, use index values.
+            (0 until dimension.length).asSequence()
+        } else if (isTime(variable)) {
+            timeValues(variable)
+        } else {
+            val iter = variable.read().indexIterator
+            generateSequence { if (iter.hasNext()) iter.objectNext else null }
+        }
+    }
 }
--- src/test/kotlin/software/mdklatt/idea/netcdf/tools/NetcdfViewerTest.kt
+++ src/test/kotlin/software/mdklatt/idea/netcdf/tools/NetcdfViewerTest.kt
@@ -5,8 +5,11 @@ package software.mdklatt.idea.netcdf.tools
 
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import org.junit.jupiter.api.Test
+import ucar.ma2.DataType
 import ucar.nc2.NetcdfFile
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 // The IDEA platform tests use JUnit3, so method names are used to determine
 // behavior instead of annotations. Notably, test classes are *not* constructed
@@ -36,5 +39,66 @@ internal class NetcdfToolWindowTest : BasePlatformTestCase() {  // JUnit3
     fun testIsApplicable() {
         assertTrue(window.isApplicable(project))
         return
+    }
+}
+
+
+/**
+ * Unit tests for the DataTableModel class.
+ */
+internal class DataTableModelTest {
+
+    private val path = "src/test/resources/sresa1b_ncar_ccsm3-example.nc"
+    private val file = NetcdfFile.open(path)
+    private val model = DataTableModel()
+
+    /**
+     * Test the rowCount property.
+     */
+    @Test
+    fun testRowCount() {
+        assertEquals(0, model.rowCount)
+        model.setData(file, sequenceOf("pr"))
+        assertEquals(32768, model.rowCount)
+    }
+
+    /**
+     * Test the columnCount property.
+     */
+    @Test
+    fun testColumnCount() {
+        assertEquals(0, model.columnCount)
+        model.setData(file, sequenceOf("pr", "tas"))
+        assertEquals(5, model.columnCount)
+    }
+
+    /**
+     * Test the getColumnName() method.
+     */
+    @Test
+    fun testGetColumnName() {
+        model.setData(file, sequenceOf("pr", "tas"))
+        assertEquals("time", model.getColumnName(0))
+        assertEquals("tas", model.getColumnName(4))
+    }
+
+    /**
+     * Test the getColumnClass() method.
+     */
+    @Test
+    fun testGetColumnClass() {
+        model.setData(file, sequenceOf("pr", "tas"))
+        assertEquals(String::class.java, model.getColumnClass(0))
+        assertEquals(Float::class.java, model.getColumnClass(4))
+    }
+
+    /**
+     * Test the getValueAt() method.
+     */
+    @Test
+    fun testGetCellValueAt() {
+        model.setData(file, sequenceOf("pr", "tas"))
+        assertEquals("2000-05-16T12:00:00Z", model.getValueAt(0, 0))
+        assertEquals(215.73935f, model.getValueAt(2, 4))
     }
 }
