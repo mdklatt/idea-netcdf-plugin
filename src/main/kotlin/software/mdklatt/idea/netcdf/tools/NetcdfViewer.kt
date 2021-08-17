3d0fdc171c41dbed662a73f2333f3e1a5747e5e3
Michael Klatt <mdklatt@alumni.ou.edu>
2021-08-17 18:02:39 -0500

    Refactored DataModelTable class to use TableView class

--- src/main/kotlin/software/mdklatt/idea/netcdf/tools/NetcdfViewer.kt
+++ src/main/kotlin/software/mdklatt/idea/netcdf/tools/NetcdfViewer.kt
@@ -244,9 +244,7 @@ private class ErrorDialog(private val message: String) : DialogWrapper(false) {
 internal class DataTableModel() : AbstractTableModel() {
 
     private val logger = Logger.getInstance(this::class.java)  // runtime class resolution
-    private var indexes = emptyList<IntArray>()
-    private var columns = mutableListOf<Column>()
-    private var labels = emptyList<String>()
+    var table : TableView? = null
 
     /**
      * Returns the number of rows in the model. A
@@ -257,7 +255,7 @@ internal class DataTableModel() : AbstractTableModel() {
      * @return the number of rows in the model
      * @see .getColumnCount
      */
-    override fun getRowCount() = indexes.size
+    override fun getRowCount() = table?.rowCount ?: 0
 
     /**
      * Returns the number of columns in the model. A
@@ -267,19 +265,18 @@ internal class DataTableModel() : AbstractTableModel() {
      * @return the number of columns in the model
      * @see .getRowCount
      */
-    override fun getColumnCount() = labels.size
-
+    override fun getColumnCount() = table?.columnCount ?: 0
 
     /**
      *
      */
-    override fun getColumnClass(columnIndex: Int): Class<*> = columns[columnIndex].type
+    override fun getColumnClass(columnIndex: Int): Class<*> = table?.column(columnIndex)?.type ?: throw IllegalStateException("empty table")
 
     /**
      * Get the name label for a column.
      *
      */
-    override fun getColumnName(column: Int) = labels[column]
+    override fun getColumnName(column: Int) = table?.column(column)?.label ?: throw IllegalStateException("empty table")
 
     /**
      * Returns the value for the cell at `columnIndex` and
@@ -289,7 +286,7 @@ internal class DataTableModel() : AbstractTableModel() {
      * @param   columnIndex     the column whose value is to be queried
      * @return  the value Object at the specified cell
      */
-    override fun getValueAt(rowIndex: Int, columnIndex: Int) = columns[columnIndex].read(indexes[rowIndex])
+    override fun getValueAt(rowIndex: Int, columnIndex: Int) = table?.column(columnIndex)?.value(rowIndex) ?: throw IllegalStateException("empty table")
 
     /**
      * Set the model data.
@@ -304,48 +301,10 @@ internal class DataTableModel() : AbstractTableModel() {
     fun setData(file: NetcdfFile, varNames: Sequence<String>) {
         // TODO: Verify that all variables have the same dimensions.
         logger.debug("Loading data from ${file.location}")
-        val variables = varNames.map {
-            file.findVariable(it) ?: throw IllegalArgumentException("Unknown variable: '${it}'")
-        }
-        val dimensions = variables.firstOrNull()?.let {
-            // If this is a fixed-length array string variable, ignore the last
-            // dimension that defines the string length.
-            if (!it.isArrayString) it.dimensions else it.dimensions.dropLast(1)
-        }?: emptyList()
-        val axes = dimensions.map { (0 until it.length) }.toTypedArray()
-        columns = dimensions.mapIndexed { axis, it -> createCoordinateColumn(file, it, axis) }.toMutableList()
-        columns.addAll(variables.map { createVariableColumn(it, dimensions.indices.toList().toIntArray()) })
-        labels = columns.map { it.label }
-        indexes = if (axes.isEmpty()) emptyList() else cartProd(*axes).map { it.toIntArray() }.toList()
+        table?.clear()
+        table = TableView(file).also { it.add(varNames) }
         fireTableStructureChanged()
         return
-    }
-
-    private fun createCoordinateColumn(file: NetcdfFile, dimension: Dimension, axis: Int) : Column {
-        val axes = intArrayOf(axis)
-        val variable = file.findVariable(dimension.fullNameEscaped)
-        return if (variable?.isCoordinateVariable != true) {
-            object : Column(dimension.fullNameEscaped) {
-                override val type = Long::class.java
-                override fun read(index: IntArray) = index[axis]
-            }
-        } else if (variable.isTime) {
-            TimeColumn(variable, axes)
-        } else if (variable.isArrayString) {
-            ArrayStringColumn(variable, axes)
-        } else {
-            DataColumn(variable, axes)
-        }
-    }
-
-    private fun createVariableColumn(variable: Variable, axes: IntArray) : Column {
-        return if (variable.isTime) {
-            TimeColumn(variable, axes)
-        } else if (variable.isArrayString) {
-            ArrayStringColumn(variable, axes)
-        } else {
-            DataColumn(variable, axes)
-        }
     }
 }
 
@@ -423,157 +382,4 @@ internal class SchemaTableModel : AbstractTableModel() {
         }.toList()
         fireTableStructureChanged()
     }
-}
-
-
-/**
- * True if variable appears to be a character array string.
- *
- * Prior to netCDF4, strings had to be stored as a 2D CHAR array where the
- * second dimension extends along the length of each string.
- *
- * @see <a href=http://www.bic.mni.mcgill.ca/users/sean/Docs/netcdf/guide.txn_58.html>Reading and Writing Character String Values</a>
- */
-private val Variable.isArrayString: Boolean
-    get() = dataType.name.toLowerCase() == "char" && shape.size == 2
-
-
-/**
- * True if variable appears to be a time variable.
- *
- * This variable is assumed to contain time values if it adheres to netCDF time
- * conventions, namely that it is a numeric variable whose name starts with
- * 'time' and has a 'units' attribute of the form '<units> since <timestamp>'
- *
- * @see <a href="https://www.unidata.ucar.edu/software/netcdf/time/recs.html">A Brief History of (netCDF) Time<a>
- */
-private val Variable.isTime : Boolean
-    get() {
-        val name = fullNameEscaped.split("/").last()
-        val regex = CalendarDateUnit.udunitPatternString.toRegex()
-        return name.startsWith("time", 0) && dataType.isNumeric && regex.matches(unitsString.toLowerCase())
-    }
-
-
-/**
- * Define a table column.
- */
-internal abstract class Column(val label: String) {
-
-    /**
-     * Data type represented by this column.
-     */
-    open val type : Class<*> = Any::class.java
-
-    /**
-     * Read a column value for the given index.
-     *
-     * @param index:
-     * @return: column value
-     */
-    abstract fun read(index: IntArray) : Any
-}
-
-
-/**
- * Table column holding a netCDF variable.
- *
- * The `axes` parameter maps table dimensions to variable dimensions, e.g. if
- * the table dataspace has dimensions ("lat", "lon"), and the variable has
- * dimensions ("lon", "lat"), the `axes` value should be [1, 0].
- *
- * @param variable: netCDF variable
- * @param axes: map table dimensions to variable dimensions
- */
-internal open class DataColumn(private val variable: Variable, private var axes: IntArray) : Column(variable.fullNameEscaped) {
-
-    /**
-     * Data type represented by this column.
-     */
-    override val type: Class<*> = variable.dataType.primitiveClassType
-
-    /**
-     * Shape required to read a single variable element.
-     */
-    protected open val shape = IntArray(variable.dimensions.size) { 1 }
-
-    /**
-     * Translate an index to an origin point for this variable. This is used in
-     * conjunction with `shape` to define the array section required to read a
-     * single value from the variable.
-     */
-    protected open fun origin(index: IntArray) = axes.map { index[it] }.toIntArray()
-
-    /**
-     * Read value(s) from the underlying netCDF variable. Child classes should
-     * override this if single column value is composed of multiple variable
-     * values.
-     *
-     * @param index: value index in the table dataspace
-     * @return: variables values(s)
-     */
-    protected fun readVariable(index: IntArray) : ucar.ma2.Array = variable.read(origin(index), shape)
-
-
-    /**
-     * Get a single column value for the given index.
-     *
-     * @param index: value index in the table dataspace
-     * @return: column value
-     */
-    override fun read(index: IntArray) : Any = readVariable(index).getObject(0)
-}
-
-
-/**
- * Column of netCDF time values.
- *
- * @see <a href="https://www.unidata.ucar.edu/software/netcdf/time/recs.html">A Brief History of (netCDF) Time<a>
- */
-internal class TimeColumn(variable: Variable, axes: IntArray) : DataColumn(variable, axes) {
-
-    private val calendar = variable.findAttribute("calendar")?.stringValue ?: Calendar.getDefault().name
-    private val units = CalendarDateUnit.of(calendar, variable.unitsString)
-
-    /** @see DataColumn.type */
-    override val type = String::class.java
-
-    /** @see DataColumn.read */
-    override fun read(index: IntArray) : Any {
-        // Override base class to convert numeric time offsets to ISO 8601
-        // timestamps.
-        val dataValue = readVariable(index)
-        val timeValue = if (dataValue.dataType.isIntegral) {
-            units.makeCalendarDate(dataValue.getObject(0).toString().toInt())
-        } else {
-            units.makeCalendarDate(dataValue.getObject(0).toString().toDouble())
-        }
-        return timeValue.toString()
-    }
-}
-
-
-/**
- * Column of fixed-length "classic" strings.
- *
- * @see <a href=http://www.bic.mni.mcgill.ca/users/sean/Docs/netcdf/guide.txn_58.html>Reading and Writing Character String Values</a>
- */
-internal class ArrayStringColumn(variable: Variable, axes: IntArray) : DataColumn(variable, axes) {
-
-    private val strLength = variable.shape.last()
-
-    /** @see DataColumn.type */
-    override val type = String::class.java
-
-    /** @see DataColumn.shape */
-    // Override base class to account for additional string length dimension.
-    override val shape = IntArray(variable.shape.size - 1) { 1 } + intArrayOf(strLength)
-
-    /** @see DataColumn.origin */
-    // Override base class to account for additional string length dimension.
-    override fun origin(index: IntArray) = super.origin(index) + intArrayOf(0)
-
-    /** @see DataColumn.read */
-    // Override base class to convert a character array to a String.
-    override fun read(index: IntArray) = readVariable(index).toString()
 }
