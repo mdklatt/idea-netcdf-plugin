e33d0182c55aa74da0dd920726cd6047c1791319
Michael Klatt <mdklatt@alumni.ou.edu>
2022-09-25 23:21:29 -0500

    Added SaveFileDialog class

---
+++ src/main/kotlin/FileDialog.kt
@@ -0,0 +1,43 @@
+package dev.mdklatt.idea.netcdf
+
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.dsl.builder.bindText
+import com.intellij.ui.dsl.builder.columns
+import com.intellij.ui.dsl.builder.panel
+import javax.swing.JComponent
+
+
+/**
+ * Modal dialog for an output file path.
+ */
+class SaveFileDialog(title: String = "Save File", private val prompt: String = "File:", default: String = "") :
+    DialogWrapper(false) {
+
+    private var path = default
+
+    init {
+        init()
+        setTitle(title)
+    }
+
+    /**
+     * Prompt the user for the password.
+     *
+     * @return user input
+     */
+    fun getPath(): String? = if (showAndGet()) path else null
+
+    /**
+     * Define dialog contents.
+     *
+     * @return: dialog contents
+     */
+    override fun createCenterPanel(): JComponent {
+        // https://plugins.jetbrains.com/docs/intellij/kotlin-ui-dsl-version-2.html
+        return panel{
+            row("${prompt}:") {
+                textFieldWithBrowseButton("File").columns(40).bindText(::path)
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/FileDialogTest.kt
@@ -0,0 +1,39 @@
+/**
+ * Test suite for FileDialogTest.kt
+ */
+package dev.mdklatt.idea.netcdf
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+
+// The IDEA platform tests use JUnit3, so method names are used to determine
+// behavior instead of annotations. Notably, test classes are *not* constructed
+// before each test, so setUp() methods should be used for initialization.
+// Also, test functions must be named `testXXX` or they will not be found
+// during automatic discovery.
+
+
+/**
+ * Unit tests for the SaveFileDialog class.
+ */
+internal class SaveFileDialogTest : BasePlatformTestCase() {
+
+    // TODO: https://github.com/JetBrains/intellij-ui-test-robot
+
+    private lateinit var dialog: SaveFileDialog
+
+    /**
+     * Per-test initialization.
+     */
+    override fun setUp() {
+        super.setUp()
+        dialog = SaveFileDialog("Save File Test")
+    }
+
+    /**
+     * Test the primary constructor.
+     */
+    fun testConstructor() {
+        assertEquals("Save File Test", dialog.title)
+    }
+}
